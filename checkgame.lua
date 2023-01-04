081acab3c993dbd5d45207140bc5cee2fd70fbd2
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2023-01-04 06:15:47 -0600

    Update checkgame.lua

--- checkgame.lua
+++ checkgame.lua
@@ -4,5 +4,5 @@ local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (f
 queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Robotboyingg2/Trying/main/checkgame.lua"))()')
 
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/test-custom-vape/main/chat%20tags.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Robotboyingg2/Trying/main/Modules/Bedwars.lua"))()
 end
