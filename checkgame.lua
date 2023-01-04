4dbd3976babae1160d07608847695c0c5db81d25
Ln3242 <109390328+Ln3242@users.noreply.github.com>
2023-01-04 05:51:41 -0600

    Update checkgame.lua

--- checkgame.lua
+++ checkgame.lua
@@ -1,10 +1,8 @@
 repeat task.wait() until game:IsLoaded()
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
-game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Robotboyingg2/Trying/main/checkgame.lua', true))()")
-    end
-end)
+local queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+
+queueteleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Robotboyingg2/Trying/main/checkgame.lua"))()')
+
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Robotboyingg2/Trying/main/Modules/Bedwars.lua"))()
 end
